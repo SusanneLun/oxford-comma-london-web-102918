4a596f4bc439b7cc31951b9e9d66c90287cc2c8e
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-25 11:03:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if array.length == 1
+  if array.length == 1 do
 array.split("")
-elsif array.length == 2 
-array.split("and")
+elsif array.length == 2 do
+array.split("and") do
 else
   array.split(",")
 end
