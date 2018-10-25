d7f353264e5c30162c8b22dd9e348d01b2b1e6d6
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-25 11:13:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,11 +3,9 @@ def oxford_comma(array)
 array.join
 elsif array.length == 2 
 array.join(" and ") 
-elsif array.length == 3
+else 
 array.join
 array[-1] = "and #{array[-1]}"
-else
-  array.join(",")
 end
 end
 
